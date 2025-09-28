2ac52663d62db14a4bfa4a22e495a1d0143662f2
Dylan Nguyen <ndquang126@gmail.com>
2025-09-28 16:58:52 -0400

    update gitignore to ignore certain files

--- run.py
+++
@@ -1,6 +0,0 @@
-from quoridor_engine import GameState, Move, negamax
-
-gs = GameState(N=9)
-print(gs.legal_moves()[:10])        # peek
-score, best = negamax(gs, depth=3, alpha=float("-inf"), beta=float("inf"))
-gs.apply(best)
